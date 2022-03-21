1de51b232c6bb56b7094470662f58a83506ae7c8
dasileker@gmail.com <dasileker@gmail.com>
2022-03-21 15:45:10 +0100

    add data

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+var selectedRow = null
+
+
 let users = [
   {
     id: "123456789",
@@ -28,29 +32,120 @@ let users = [
   }
 ]
 
+// document.getElementById('add').addEventListener("click", function () {
+//   window.localStorage.setItem('formData', JSON.stringify(id: ));
+//   // insertNewRecord();
+// })
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+function onFormSubmit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+static(users);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function static(data){
+  var entry = document.getElementById('myTable');
+
+  for (var i = 0; i < data.length; i++){
+    var row = `<tr><td>${data[i].id}</td><td>${data[i].createdDate}</td><td>${data[i].status}</td><td>${data[i].firstName}</td><td>${data[i].lastName}</td><td>${data[i].userName}</td><td>${data[i].registrationNumber}</td><td><button onClick="onEdit(this)">Edit</button> <i  onClick="onDelete(this)" class="fa fa-trash-o" style="font-size:20px;color:red"></td></tr>`
+
+    entry.innerHTML += row;
   }
+
 }
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData ["id"] = document.getElementById('ID');
+    // id.textContent = Date.now();
+    formData["firstName"] = document.getElementById("firstName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["createdDate"] = document.getElementById("createdDate").value;
+    formData["Etat"] = document.getElementById("Etat").value;
+    formData["userName"] = document.getElementById("userName").value;
+    formData["registrationNumber"] = document.getElementById("registrationNumber").value;
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = `${Date.now()}`;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.createdDate;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.Etat;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.firstName;
+    cell5 = newRow.insertCell(4);
+		cell5.innerHTML = data.lastName;
+    cell6 = newRow.insertCell(5);
+		cell6.innerHTML = data.userName;
+    cell7 = newRow.insertCell((6));
+		cell7.innerHTML = data.registrationNumber;
+    cell8 = newRow.insertCell((7));
+    cell8.innerHTML = `<button onClick="onEdit(this)">Edit</button> <i  onClick="onDelete(this)" class="fa fa-trash-o" style="font-size:20px;color:red">`;
+
+  
+    
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+     document.getElementById("id") = selectedRow.cells[0].innerHTML;
+    document.getElementById("createdDate").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Etat").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("firstName").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("lastName").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("userName").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("registrationNumber").value = selectedRow.cells[6].innerHTML;
+}
+function updateRecord(formData) {
+    // selectedRow.cells[0].innerHTML = cell1;
+    selectedRow.cells[1].innerHTML = formData.createdDate;
+    selectedRow.cells[2].innerHTML = formData.Etat;
+    selectedRow.cells[3].innerHTML = formData.firstName;
+    selectedRow.cells[4].innerHTML = formData.lastName;
+    selectedRow.cells[5].innerHTML = formData.userName;
+    selectedRow.cells[6].innerHTML = formData.registrationNumber;
+}
+
+//Delete the data
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function resetForm() {
+    // document.getElementById("ID").innerHTML = `${Date.now()}`;
+    document.getElementById("createdDate").value = '';
+    document.getElementById("firstName").value = '';
+    document.getElementById("Etat").value = '';
+    document.getElementById("lastName").value = '';
+    document.getElementById("userName").value = '';
+    document.getElementById("registrationNumber").value = '';
+    selectedRow = null;
+}
+
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+
